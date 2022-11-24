95042289957931ce50a158afb1448dd8319831b1
ACER <ACER@DESKTOP-O7A416V>
2022-11-24 17:20:59 +0300

    Добавьте файлы проекта.

---
+++ Практика 11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ConsoleWhile
+{
+    class Program
+    {
+        static void Main()
+        {
+            double a = 0, e = 0.001, q = 0, s = 0, x = 0.15;
+            int n = 0;
+            a = x;
+            s = a;
+            while (Math.Abs(a) > e)
+            {
+                q = -x * x / (4 * n * n + 1) / (4 * n * n + 8 * n + 5);
+                a *= q;
+                s += a;
+                n++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Результат");
+            Console.WriteLine();
+            Console.WriteLine("Сумма s=" + Convert.ToString(s));
+            Console.WriteLine("Число членов ряда n=" + Convert.ToString(n));
+            Console.WriteLine();
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
